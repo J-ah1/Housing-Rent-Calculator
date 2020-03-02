74af923130fec1e17dac2f4305ff4387c881cf35
Felix Andrew Sapalaran <40646937+felixandrewsapalaran@users.noreply.github.com>
2020-03-02 01:55:38 -0500

    Add files via upload

--- Client/src/Components/Register.js
+++ Client/src/Components/Register.js
@@ -1,245 +1,137 @@
-import React, { Component } from 'react';
-import './Register.css';
-
-const emailRegex = RegExp(
-  /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
-  );
-
-const formValid =  ({formErrors, ...rest}) => {
-  let valid = true;
-
-  //validate form errors  being empty
-  Object.values(formErrors).forEach(val => {
-    val.length > 0 && (valid = false)
-  });
+import React, {Component} from 'react';
 
 
-//validate the form was filled out
-  Object.values(rest).forEach(val => {
-    val == null && (valid = false)
-  });
+class Register extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            username: "",
+            password: "",
+            errorMessage: ""
+        }
+    }
 
+    nameChange = (e) => {
+        this.setState({
+            username: e.target.value
+        })
+    }  // function is used for fill the default states
 
-  return valid;
+    fNameChange = (e) => {
+        this.setState({
+            fName: e.target.value
+        })
+    } // function is used for fill the default states
+
+    lNameChange = (e) => {
+        this.setState({
+            lName: e.target.value
+        })
+    }// function is used for fill the default states
+
+    pswdChange = (e) => {
+        this.setState({
+            pswd: e.target.value
+        })
+    }// function is used for fill the default states
+
+    cpswdChange = (e) => {
+        this.setState({
+            cpswd: e.target.value
+        })
+    }// function is used for fill the default states
+
+    emailChange = (e) => {
+        this.setState({
+            email: e.target.value
+        })
+    }// function is used for fill the default states
+
+    phoneChange = (e) => {
+        this.setState({
+            phone: e.target.value
+        })
+    }// function is used for fill the default states
+
+    submit = (e) => {
+        e.preventDefault();
+        //if condition is check the field is empty or not
+        if(this.state.username !== "" && this.state.pswd !== "" && this.state.fName !== "" &&
+        this.state.phone !== "" && this.state.email !== "" && this.state.cpswd !=="" && this.state.lName !=="" ){
+            var request = new XMLHttpRequest();//create object for ajax calling
+            request.open("POST", "http://localhost:8500/rentcalculator/index.cfm/register",false);//set ajax call url
+            request.setRequestHeader("Content-Type", "application/json");//set header
+            request.send(JSON.stringify({"username":this.state.username,"fName":this.state.fName,"phone":this.state.phone,
+            "email":this.state.email,"cpswd":this.state.cpswd,"pswd":this.state.pswd,"lName":this.state.lName}));//set body with json type
+            const resposedata=JSON.parse(request.responseText);// request.responseText response which gives ajax calling
+            if(resposedata.status===true){
+                alert("login:  "+resposedata.message);
+                this.setState({errorMessage:""})
+                this.props.handler();//this function handle the login
+
+            }else{
+
+                this.setState({errorMessage:resposedata.message})//set the errorMessage when ajax response error
+            }
+
+        }
+        else {
+            this.setState({errorMessage: "All Field  Cannot be Empty"})// when field are empty
+
+        }
+
+    }
+    render() {
+        return (
+            <div id="mydiv">
+                <h1>Create Your Profile</h1>
+                <div className={ this.state.errorMessage?"alert":"" }>
+                {
+                    this.state.errorMessage &&
+                     this.state.errorMessage
+                }
+                </div>
+                 <form onSubmit={this.submit}>
+                    <table align="center">
+                        <tbody>
+                        <tr>
+                            <th><label>Username:</label></th>
+                            <td><input  value={this.state.username} onChange={this.nameChange} type="text"  required/></td>
+                        </tr>
+                        <tr>
+                            <th><label>First Name:</label></th>
+                            <td><input  value={this.state.fName} onChange={this.fNameChange} type="text" required/></td>
+                        </tr>
+                        <tr>
+                            <th><label>Last Name:</label></th>
+                            <td><input value={this.state.LName} onChange={this.lNameChange} type="text" required/></td>
+                        </tr>
+                        <tr>
+                            <th><label>Password:</label></th>
+                            <td><input value={this.state.pswd} onChange={this.pswdChange} type="password" required /></td>
+                        </tr>
+                        <tr>
+                            <th><label>Confirm Password:</label></th>
+                            <td><input value={this.state.cpswd} onChange={this.cpswdChange} type="password" required /></td>
+                        </tr>
+                        <tr>
+                            <th><label>Email:</label></th>
+                            <td><input value={this.state.email} onChange={this.emailChange} type="email" required /></td>
+                        </tr>
+                        <tr>
+                            <th><label>Phone Number:</label></th>
+                            <td><input value={this.state.phone} onChange={this.phoneChange} type="text" required/></td>
+                        </tr>
+                        <tr>
+                            <td colSpan="2"><input type="submit" value="SIGN UP" required/></td>
+                        </tr>
+
+                    </tbody>
+                    </table>
+                 </form>
+
+            </div>
+        )
+    }
 }
-
-class Register extends Component{
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      username: null,
-      firstName: null,
-      lastName: null,
-      email: null,
-      phone: null,
-      password: null,
-      formErrors: {
-        username: "",
-        firstName: "",
-        lastName: "",
-        email:"",
-        phone:"",
-        password:""
-
-      }
-    };
-  }
-
-handleSubmit = e => {
-  //stop the form from submitting
-  e.preventDefault();
-
-  // if(formValid(this.state.formErrors)){
-    if(formValid(this.state)){
-    console.log(`
-      --SUBMITTING--
-      Username: ${this.state.username}
-      First Name: ${this.state.firstName}
-      Last Name: ${this.state.lastName}
-      Email Name: ${this.state.email}
-      Phone Number: ${this.state.phone}
-      Password: ${this.state.password}
-    `)
-  }else {
-    console.error('FORM INVALID - DISPLAY ERROR MESSAGE');
-  }
-};
-
-
-handleChange = e => {
-  e.preventDefault();
-  // const { name } = e.target;
-  // const { value } = e.target;
-  const { name, value} = e.target;
-  let formErrors = this.state.formErrors;
-
-  // console.log("Name: ", name);
-  // console.log("value: ", value);
-
-  switch (name) {
-    case 'username':
-      formErrors.username = value.length < 3 && value.length > 0
-      ? "minimum 3 characters required"
-      : "";
-      break;
-
-    case 'firstname':
-      formErrors.firstName = value.length < 3 && value.length > 0
-      ? "minimum 3 characters required"
-      : "";
-      break;
-
-      case 'lastname':
-      formErrors.lastName = value.length < 3 && value.length > 0
-      ? "minimum 3 characters required"
-      : "";
-      break;
-
-      case 'email':
-      formErrors.email =
-      emailRegex.test(value)
-      ? ""
-      : "invalid email address";
-      break;
-
-      case 'phone':
-      formErrors.phone = value.length <10 && value.length > 0
-      ? "minimum 10 characters required"
-      : "";
-      break;
-
-      case 'password':
-      formErrors.password = value.length <6 && value.length > 0
-      ? "minimum 6 characters required"
-      : "";
-      break;
-    default:
-      break;
-  }
-
-  this.setState({formErrors, [name]: value}, () => console.log(this.state))
-};
-
-  render() {
-
-    const {formErrors} = this.state;
-
-    return (
-      <div className="wrapper">
-        <div className= "form-wrapper">
-          <h1>Create Account</h1>
-          <form onSubmit={this.handleSubmit} noValidate>
-
-
-          <div className="username">
-              <label htmlFor="username">Username</label>
-              <input
-              className={formErrors.username.length > 0 ? "error" : null}
-              placeholder = "username"
-              type = "text"
-              name = "username"
-              onChange = {this.handleChange}
-              noValidate/>
-
-              {formErrors.username.length > 0 && (
-              <span className = "errorMessage">{formErrors.username}</span>
-            )}
-
-            </div>
-            <div className="firstName">
-              <label htmlFor="firstName">First Name</label>
-              <input
-              className={formErrors.firstName.length > 0 ? "error" : null}
-              placeholder = "First Name"
-              type = "text"
-              name = "firstname"
-              onChange = {this.handleChange}
-              noValidate/>
-
-              {formErrors.firstName.length > 0 && (
-              <span className = "errorMessage">{formErrors.firstName}</span>
-            )}
-
-            </div>
-
-            <div className="lastName">
-              <label htmlFor="lastName">Last Name</label>
-              <input
-              className= {formErrors.lastName.length > 0 ? "error" : null}
-              placeholder = "Last Name"
-              type = "text"
-              name = "lastname"
-              onChange = {this.handleChange}
-              noValidate/>
-
-              {formErrors.lastName.length > 0 && (
-              <span className = "errorMessage">{formErrors.lastName}</span>
-            )}
-            </div>
-
-
-
-            <div className="email">
-              <label htmlFor="firstName">Email</label>
-              <input
-              className= {formErrors.email.length > 0 ? "error" : null}
-              placeholder = "email"
-              type = "email"
-              name = "email"
-              onChange = {this.handleChange}
-              noValidate/>
-
-              {formErrors.email.length > 0 && (
-              <span className = "errorMessage">{formErrors.email}</span>
-            )}
-            </div>
-
-
-            <div className="phone">
-              <label htmlFor="phone">phone</label>
-              <input
-              className={formErrors.phone.length > 0 ? "error" : null}
-              placeholder = "phone"
-              type = "text"
-              name = "phone"
-              onChange = {this.handleChange}
-              noValidate/>
-
-              {formErrors.phone.length > 0 && (
-              <span className = "errorMessage">{formErrors.phone}</span>
-            )}
-
-            </div>
-
-
-
-
-            <div className="password">
-              <label htmlFor="password">Password</label>
-              <input
-              className= {formErrors.password.length > 0 ? "error" : null}
-              placeholder = "password"
-              type = "password"
-              name = "password"
-              onChange = {this.handleChange}
-              noValidate/>
-
-            {formErrors.password.length > 0 && (
-              <span className = "errorMessage">{formErrors.password}</span>
-            )}
-            </div>
-
-            <div className = "createAccount">
-              <button type = "submit"> Create Account</button>
-              {/* <small>Already Have an Account?</small> */}
-            </div>
-
-          </form>
-        </div>
-      </div>
-    );
-  }
-}
-
-export default Register;
+export default Register
