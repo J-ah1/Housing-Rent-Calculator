8f1e1d98958f2ce9b793725532cf0219bb423347
Keirsten <33580138+Keirsten997@users.noreply.github.com>
2020-03-19 10:35:17 -0400

    Update RentCalculator0.js

--- Client/src/Components/RentCalculator0.js
+++ Client/src/Components/RentCalculator0.js
@@ -1,44 +1,20 @@
-import React, { Component } from 'react';
+import React from 'react';
+//import {Link, Redirect} from 'react-router-dom';
 
-class RentCalculator0 extends Component {
-    clickedStart(){
-        console.log('Take the user to the first page of the rent calculator.')
-        //redirect to first page of rent calculator
-        /*this.props.history.push({
-            pathname: '/rc1',
-        })*/
-    }
-
-    clickedBack(){
-        console.log('Take the user back to the client worksheets page.')
-        //this needs to be changed so that it redirects to the profile page with client ID
-        this.props.history.push({
-            pathname: '/profile/${id}',
-        })
-    }
-
-    clickedLogOff(){
-        console.log('Log the user off.')
-        this.props.history.push({
-            pathname: '/',
-        })
-    }
-
-    render(){
-        return(
-            <div align="center">
-                    <button onClick = { (e) => {this.clickedBack() } } style={{float : 'left', paddingRight : '5px'}}>Back</button>
-                    <button onClick = { (e) => {this.clickedLogOff() } } style={{float : 'right', paddingRight : '5px'}}>Sign Out</button>
-                    <h1>Welcome to the Rent Calculator </h1>
-                    <p>This is where the disclaimer text will go.
-                        You're about start the rent calculator.
-                        Please handle the client's information properly.
-                        ....
-                    </p>
-                    <button onClick = { (e) => {this.clickedStart() } } >Start</button>
-            </div>
-            
-        )
-    }
+function RentCalculator0(props){ 
+    return(
+        <div align="center">
+                {/* <button onClick = { (e) => {props.clickedBack() } } style={{float : 'left', paddingRight : '5px'}}>Back</button> */}
+                <button type = "button" onClick = {props.backHandler} style={{float : 'left', paddingRight : '5px'}}>Back</button>
+                <button type = "button" onClick = { props.logOffHandler} style={{float : 'right', paddingRight : '5px'}}>Sign Out</button>
+                <h1>Welcome to the Rent Calculator </h1>
+                <p>This is where the disclaimer text will go.
+                    You're about start the rent calculator.
+                    Please handle the client's information properly.
+                    ....
+                </p>
+                <button type = "button" onClick = {props.startHandler} >Start</button>
+        </div>    
+    )  
 }
 export default RentCalculator0;
