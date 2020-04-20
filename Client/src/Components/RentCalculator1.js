d2f4968b0bee30549ac102a8736484fd50be3ba4
Henry Casey <hpc3@njit.edu>
2020-04-19 22:25:05 -0400

    Modified: refactored for flexbox

--- Client/src/Components/RentCalculator1.js
+++ Client/src/Components/RentCalculator1.js
@@ -7,18 +7,18 @@ function questionType(props, type, count){
     let input
     switch(type){
         case("radio"):
-            input = <div>
-                    <input type="radio" id={count} name={count} onChange={props.inputHandler} checked={props.results[count] === "Yes" ? true : false}  value="Yes" />
-                    <label htmlFor="Yes">Yes</label>
-                    <input type="radio" id={count} name={count} onChange={props.inputHandler} checked={props.results[count] === "No" ? true : false} value="No" />
-                    <label htmlFor="No">No</label>
-                </div>
+            input = <div style={{display: 'flex'}}>
+                        <input className="mr-1 mt-1" type="radio" id={count} name={count} onChange={props.inputHandler} checked={props.results[count] === "Yes" ? true : false}  value="Yes" />
+                        <label className="mr-3" htmlFor="Yes">Yes</label>
+                        <input className="mr-1 mt-1" type="radio" id={count} name={count} onChange={props.inputHandler} checked={props.results[count] === "No" ? true : false} value="No" />
+                        <label className="mr-4" htmlFor="No">No</label>
+                    </div>
             break;
         case("date"):
             input = <input onChange={props.inputHandler} id={count} value={props.results[count]} type="date"  />
             break;
         default:
-            input = <input onChange={props.inputHandler} id={count} value={props.results[count]} type="number" min="0" />
+            input = <input className="rounded" onChange={props.inputHandler} id={count} value={props.results[count]} type="number" min="0" />
             break;
     }
     return input
@@ -34,40 +34,34 @@ export default function RentCalculator1(props){
     let count = -1;
 
     return(
-        <div align="center">
+        <div className="rent-calc-container" align="center">
                 {/* Here is where you add the contents of what will be displayed to screen */}
-
-                {/* <button type = "button" onClick = { props.logOffHandler} style={{float : 'right', paddingRight : '5px'}}>Sign Out</button> */}
+                {/* <button type = "button" onClick = { props.logOffHandler} style={{float : 'right', paddingRight : '5px'}}>Sign Out</button> */}        
                 <h1>Gross Household Income</h1>
-                <table>
-                    <thead>
-                        <tr>
-                            <th>Question</th>
-                            <th>Description</th>
-                            <th>Response</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {questions.map(question => {
+                <div className="rent-calc-container-content">
+                {questions.map(question => {
                             count++
-                            return (<tr key={count}>
-                                <td>{question.label}</td>
-                                <td>{question.description}</td>
-                                <td>{questionType(props, question.type, count)}</td>
-                            </tr>)
-                        })}
-                    </tbody>
-                    <tfoot>
-                        <tr>
-                            <td>{results[0].label}</td>
-                            <td><input value={props.total1} readOnly/></td>
-                        </tr>
-                        <tr>
-                            <td>{results[1].label}</td>
-                            <td><input value={props.total2} readOnly/></td>
-                        </tr>
-                    </tfoot>
-                </table>
+                            return (<div className="rent-calc-question-container" key={count}>
+                                <div className="rent-calc-question-label-description">
+                                    <p className="rent-calc-label">{count + 1}. {question.label}</p>
+                                    <p className="rent-calc-description">{question.description}</p>
+                                </div>
+                                <div>
+                                {questionType(props, question.type, count)}
+                                </div>
+                            </div>)
+                })}
+                    <div className="rent-calc-question-container mt-3 mb-4">
+                        <p className="rent-calc-label">{results[0].label}</p>
+                        <input className="rounded" value={props.total1} readOnly/>
+                    </div>
+                    <div  className="rent-calc-question-container">
+                        <p className="rent-calc-label">{results[1].label}</p>
+                        <input className="rounded" value={props.total2} readOnly/>
+                    </div>
+                </div>
+
+                
         </div>    
     )  
 }
